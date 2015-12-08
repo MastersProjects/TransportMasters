0db9f36ae36a9db3f50d6ee14e012e2596e17009
zperee <elia.perenzin@bluewin.ch>
2015-12-08 19:10:31 +0100

    first commit error :tada:
    Signed-off-by: zperee <elia.perenzin@bluewin.ch>

---
+++ model/JourneyDetails.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * This class is used for writing the details about the
+ * mode of transport in a Journey. One Journey {@link Journey}
+ * has 2 JourneyDetails.
+ * @author Chiramed Phong Penglerd, Luca Marti, Elia Perenzin
+ * @version 1.0
+ * TransportMasters 2015
+ */
+
+Class JourneyDetails{
+	private $time;
+	private $platform;
+	private $location;
+}
