ab551d4160faecca0e866189b8dac66c6f8d9578
zperee <elia.perenzin@bluewin.ch>
2015-12-08 19:10:31 +0100

    created model from UML
    Signed-off-by: zperee <elia.perenzin@bluewin.ch>

---
+++ model/Connection.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This Class is used for writing the information's received
+ * from the JSON into an PHP object. This is an modell Class,
+ * see also UML
+ * @author Chiramed Phong Penglerd, Luca Marti, Elia Perenzin
+ * @version 1.0
+ * TransportMasters 2015
+ */
+require_once 'Journey.php';
+
+Class Connection{
+	private $from;
+	private $to;
+	private $duration;
+	private $journey;
+	private $departureTime;
+	private $arivallTime;
+	private $transfers;	
+	
+	function __construct($from, $to, $duration, $journey, $departureTime, $arivallTime, $transfers) {
+		$this->journey = new Journey();
+	}
+}
---
+++ model/Journey.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This class is used for writing the information's of the different 
+ * mode of transport in an connection into an PHP object. In one 
+ * Connetcion {@link Connection} it has at least one Journey object.
+ * @author Chiramed Phong Penglerd, Luca Marti, Elia Perenzin
+ * @version 1.0
+ * TransportMasters 2015
+ */
+Class Connection{
+	private $name;
+	private $departure;
+	private $arrival;
+	private $category;
+	
+	function __construct() {
+		$this->arrival = new JourneyDetails();
+		$this->departure = new JourneyDetails();
+	}
+}
