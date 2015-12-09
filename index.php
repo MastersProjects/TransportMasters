5122d4024866b4571937cf1c78954e7b6e9053b4
zperee <elia.perenzin@bluewin.ch>
2015-12-09 22:08:45 +0100

    index.php ToDo change color
    Signed-off-by: zperee <elia.perenzin@bluewin.ch>

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+<title>TransportMasters</title>
+<link rel="stylesheet"
+	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="style/style.css">
+
+</head>
+<body>
+	<div class="imgheader">
+		<div class="formbackground">
+			<div class="form">
+				<form>
+					<input type="text" name="von" placeholder="Von" /> 
+					<input type="text" name="bis" placeholder="Bis" />
+					<input type="submit" value="Send" />
+				</form>
+			</div>
+		</div>
+		<div class="buttonBottom">
+			<i class="fa fa-chevron-circle-down fa-3x"></i>
+		</div>
+	</div>
+</body>
+</html>
