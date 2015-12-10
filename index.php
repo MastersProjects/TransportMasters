c87ed674f026f6c96b0d20a40fc4ede4d57068e2
zperee <elia.perenzin@bluewin.ch>
2015-12-10 20:56:32 +0100

    about us added but style bug

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <html>
 <head>
 <title>TransportMasters</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 <!-- import style sheet for the icons -->
 <link rel="stylesheet"
 	href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 <link rel="stylesheet" href="style/style.css">
 <link rel="stylesheet" href="style/form.css">
+
 <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="//code.jquery.com/jquery-1.10.2.js"></script>
+<script src="style/randomimg.js"></script>
 
 </head>
 <body>
@@ -25,13 +30,40 @@
 		</div>
 		<div class="buttonBottom">
 			<a href="#test" class="click"><i
-				class="fa fa-chevron-circle-down fa-3x" style="color: #1D50E8"></i></a>
+				class="fa fa-chevron-circle-down fa-3x" style="color: #1D50E8"></i>
+			</a>
 		</div>
 	</div>
 
 	<div id="test"></div>
-
+	<div id="aboutus">
+		<div class="about">
+		<div class="aboutperson">
+			<img class="img-about" src="img/luca.png" alt="Luca Marti">
+			<h3>
+				Luca Marti <small>Apprentice</small><br><small>Z&uuml;rich Airport</small>
+			</h3>
+			<p>What does this team member to? Keep it short! This is also a great
+				spot for social links!</p>
+		</div>
+		<div class="aboutperson">
+			<img class="img-about" src="img/phong.png" alt="Chiramed Phong Penglerd">
+			<h3>
+				Chiramed Phong Penglerd <small>Apprentice</small><small> Raiffeisen</small>
+			</h3>
+			<p>What does this team member to? Keep it short! This is also a great
+				spot for social links!</p>
+		</div>
+		<div class="aboutperson">
+			<img class="img-about" src="img/elia.png" alt="Elia Perenzin">
+			<h3>
+				Elia Perenzin <small>Apperntice</small><br><small>Hewlett-Packard Enterprise</small>
+			</h3>
+			<p>What does this team member to? Keep it short! This is also a great
+				spot for social links!</p>
+		</div>
+		</div>
+	</div>
 	<script src="style/scroll.js"></script>
-
 </body>
 </html>
---
+++ style/randomimg.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+//the highest number of the image you want to load
+var upperLimit = 3;
+
+//get random number between 1 and 3
+//change upperlimit above to increase or 
+//decrease range
+var randomNum = Math.floor((Math.random() * upperLimit) + 1);    
+
+ //change the background image to a random jpg
+ //edit add closing )  to prevent syntax error
+ $(".imgheader").css("background-image","url('img/background_0" + randomNum + ".jpg')");//<--changed path
+
+ });
